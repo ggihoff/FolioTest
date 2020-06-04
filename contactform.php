9c8fb30bef340d6e9dc5b55b50984374bc8c0b17
Gustavo iglesias <ggihoff@gmail.com>
2020-06-04 11:57:46 -0300

    prueba

--- contactform.php
+++ contactform.php
@@ -3,31 +3,5 @@
 
 
 
-if(isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = 'trabajo';
-    $mail = $_POST['mail'];
-    $message = $_POST['message']; 
-    
-    $youremail = 'gustavoiglesias_04@hotmail.com';
-    $to = $youremail;
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
-    $headers .= "From: ".$mail;
-    $body = "Recibiste un mail de ".$name.".\n\n".$message;
-    mail($to, $subject, $body, $headers);
-    
-
-    if (mail($to, $subject, $body, $headers)) {
-        echo("
-           Message successfully sent!
-        ");
-    }
-    else {
-        echo("
-           Message delivery failed...
-        ");
     header("Location: index.php?mailsend");
 
-} 
-?>
