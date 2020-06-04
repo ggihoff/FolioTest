892dff90b344bbd6e56d0e15dbc23505573e4c92
Gustavo iglesias <ggihoff@gmail.com>
2020-06-04 12:14:26 -0300

    ksadsa

--- contactform.php
+++ contactform.php
@@ -2,11 +2,10 @@
 <?php
 
 ini_set('display_errors', 1);
-
-
+echo "hiasas";
 if(isset($_POST['submit'])) {
     
-    console.log("hola")
+    console.log("hola");
     
 
     header("Location: index.php");
