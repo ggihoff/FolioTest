0d540f8aecd224cd684e96d39ac8c250e5f6cf66
Gustavo iglesias <ggihoff@gmail.com>
2020-06-04 03:37:09 -0300

    first commit

---
+++ contactform.php
@@ -0,0 +1,20 @@
+
+<?php
+
+
+if(isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = 'trabajo';
+    $mail = $_POST['mail'];
+    $message = $_POST['message']; 
+    
+    $youremail = 'gustavoiglesias_04@hotmail.com';
+    $to = $youremail;
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
+    $headers .= "From: ".$mail;
+    $txt = "Recibiste un mail de ".$name.".\n\n".$message;
+    mail($to, $subject, $txt, $headers);
+
+    header("Location: index.php?mailsend");
+} 
--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,37 @@
 var controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({
-    triggerElement : '.title1'
+  triggerElement: ".title1",
 })
-.setClassToggle('.title1', 'show')
-.addTo(controller);
+  .setClassToggle(".title1", "show")
+  .addTo(controller);
 
 var scene2 = new ScrollMagic.Scene({
-    triggerElement : '.cards'
+  triggerElement: ".cards",
 })
-.setClassToggle('.cards', 'show')
-.addTo(controller);
+  .setClassToggle(".cards", "show")
+  .addTo(controller);
 
 var scene3 = new ScrollMagic.Scene({
-    triggerElement : '.title2'
+  triggerElement: ".title2",
 })
-.setClassToggle('.title2', 'show')
-.addTo(controller);
+  .setClassToggle(".title2", "show")
+  .addTo(controller);
 
 var scene4 = new ScrollMagic.Scene({
-    triggerElement : '.form'
+  triggerElement: ".form",
 })
-.setClassToggle('.form', 'show')
-.addTo(controller);
+  .setClassToggle(".form", "show")
+  .addTo(controller);
 
+var scene6 = new ScrollMagic.Scene({
+  triggerElement: ".title3",
+})
+  .setClassToggle(".title3", "show")
+  .addTo(controller);
+
+var scene7 = new ScrollMagic.Scene({
+  triggerElement: ".sobreMi",
+})
+  .setClassToggle(".sobreMi", "show")
+  .addTo(controller);
